4336b4a45578daf44d4af332ec1ac2308ce72775
samiulalimsaad <saadnbiuraj@gmail.com>
2022-09-06 17:55:37 +0600

    `7.9` added

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,25 @@
+import { useSelector } from "react-redux";
+
 export default function Balance() {
+    const { transactions } = useSelector((state) => state.transaction);
+
+    let balance = 0;
+
+    transactions.map((t) => {
+        if (t.type === "income") {
+            balance += t.amount;
+        }
+        if (t.type === "expense") {
+            balance -= t.amount;
+        }
+    });
+
     return (
         <div className="top_card">
             <p>Your Current Balance</p>
             <h3>
                 <span>৳</span>
-                <span>10500</span>
+                <span>{balance.toLocaleString()}</span>
             </h3>
         </div>
     );
